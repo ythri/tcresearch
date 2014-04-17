77225b8b99df2464cb53ca486b90ba239a0ce33c
Ben Williams <ben@smash.mn>
2014-04-17 09:45:45 +0100

    Make a few more changes: Add 4.1.1.9 information, from the Thaumcraft API (https://github.com/Azanor/thaumcraft-api/blob/master/aspects/Aspect.java). Make addons more modular, including making an addon dictionary. Change sort to be according to the API. Actually make version change work by resetting the graph. Probably even more stuff that I thought was cool, but I forgot about.

--- tcresearch.js
+++ tcresearch.js
@@ -1,8 +1,11 @@
 $(function(){
+	var latest_version = "4.1.1.9";
 	$.each(version_dictionary, function(key,version){
 		$("#version").append("<option value="+key+">"+key+"</option>");
 	});
-	var latest_version = "4.1.0g";
+	var aspects = [];
+	var addon_aspects;
+	var combinations = {};
 	$("#version").val(latest_version);
 	var version=latest_version;
 	graph = {};
@@ -13,14 +16,6 @@ $(function(){
 		}
 		addConnection(aspect1, aspect2);
 		addConnection(aspect2, aspect1);
-	}
-
-	function output(path) {
-		var output = [];
-		path.forEach(function(e) {
-			output.push(translate[e] + " (" + e + ")");
-		});
-		return output.join(" -> ");
 	}
 
 	function find(from, to, steps) {
@@ -50,16 +45,29 @@ $(function(){
 		visited = {};
 		return search(queue, to, visited);
 	}
-	translate = { "air": "aer", "earth": "terra", "fire": "ignis", "water": "aqua", "order": "ordo", "entropy": "perditio", "void": "vacuos", "light": "lux", "energy": "potentia", "motion": "motus", "stone": "saxum", "life": "victus", "weather": "tempestas", "ice": "gelum", "crystal": "vitreus", "death": "mortuus", "flight": "volatus", "darkness": "tenebrae", "soul": "spiritus", "heal": "sano", "travel": "iter", "poison": "venenum", "eldritch": "alienis", "magic": "praecantatio", "aura": "auram", "taint": "vitium", "seed": "granum", "slime": "limus", "plant": "herba", "tree": "arbor", "beast": "bestia", "flesh": "corpus", "undead": "exanimis", "mind": "cognitio", "senses": "sensus", "man": "humanus", "crop": "messis", "harvest": "meto", "metal": "metallum", "mine": "perfodio", "tool": "instrumentum", "weapon": "telum", "armor": "tutamen", "hunger": "fames", "greed": "lucrum", "craft": "fabrico", "cloth": "pannus", "mechanism": "machina", "trap": "vinculum", "exchange": "permutatio", "wrath": "ira", "nether": "infernus", "gluttony": "gula", "envy": "invidia", "sloth": "desidia", "pride": "superbia", "lust": "luxuria", "time": "tempus" };
-	fm = [ "wrath", "nether", "gluttony", "envy", "sloth", "pride", "lust" ]
-	mb = [ "time" ];
-	for (compound in version_dictionary[version]["combinations"]) {
-		connect(compound, version_dictionary[version]["combinations"][compound][0]);
-		connect(compound, version_dictionary[version]["combinations"][compound][1]);
+	function push_addons(aspects, combinations) {
+		addon_aspects = [];
+		$.each(addon_dictionary, function(key, addon_info){
+			$("#addons").append('<input type="checkbox" class="addon_toggle" id="'+key+'" /> <label for="'+key+'">'+addon_info["name"]+'</label>');
+			$.each(addon_info["aspects"], function(number, aspect){
+				aspects.push(aspect);
+				addon_aspects.push(aspect);
+			});
+			$.each(addon_info["combinations"], function(combination_name, combination){
+				combinations[combination_name]=combination;
+			});
+		});
 	}
 	function toggle(obj) {
 		$(obj).find("img").attr("src", function(i,orig){ return (orig.indexOf("color") < 0) ? orig.replace(/mono/, "color") : orig.replace(/color/, "mono"); });
 		$(obj).toggleClass("unavail");
+	}
+	function toggle_addons(aspect_list){
+		aspect_list.forEach(function(e){
+			var obj = $('#'+e);
+			obj.find("img").attr("src", function(i,orig){ return orig.replace(/color/, "mono"); });
+			obj.addClass("unavail");
+		});
 	}
 	function option(value, text) {
 		var option = document.createElement("option");
@@ -72,37 +80,23 @@ $(function(){
 	toSel = document.getElementById("toSel");
 	check = document.getElementById("available");
 	steps = $("#spinner").spinner({
-		min: 0,
+		min: 1,
 		max: 10
 	});
 	reset_aspects();
 	$("#find_connection").click(function(){
 		run();
 	});
-	$('#fm').click(function() {
+	$('#addons').on("change", ".addon_toggle", function() {
+		addon = $(this).attr("id");
 		if (this.checked) {
-			fm.forEach(function(e){
+			addon_dictionary[addon]["aspects"].forEach(function(e){
 				var obj = $('#'+e);
 				obj.find("img").attr("src", function(i,orig){ return orig.replace(/mono/, "color"); });
 				obj.removeClass("unavail");
 			});
 		} else {
-			fm.forEach(function(e){
-				var obj = $('#'+e);
-				obj.find("img").attr("src", function(i,orig){ return orig.replace(/color/, "mono"); });
-				obj.addClass("unavail");
-			});
-		}
-	});
-	$('#mb').click(function() {
-		if (this.checked) {
-			mb.forEach(function(e){
-				var obj = $('#'+e);
-				obj.find("img").attr("src", function(i,orig){ return orig.replace(/mono/, "color"); });
-				obj.removeClass("unavail");
-			});
-		} else {
-			mb.forEach(function(e){
+			addon_dictionary[addon]["aspects"].forEach(function(e){
 				var obj = $('#'+e);
 				obj.find("img").attr("src", function(i,orig){ return orig.replace(/color/, "mono"); });
 				obj.addClass("unavail");
@@ -114,19 +108,18 @@ $(function(){
 			$(this).find("img").attr("src", function(i,orig){ return orig.replace("mono", "color")});
 			$(this).removeClass("unavail");
 		});
-		$("#fm").prop('checked', true);
-		$("#mb").prop('checked', true);
+		$(".addon_toggle").prop('checked', true);
 	});
 	$("#desel_all").click(function(){
 		$(".aspect").each(function(){
 			$(this).find("img").attr("src", function(i,orig){ return orig.replace("color", "mono")});
 			$(this).addClass("unavail");
 		});
-		$("#fm").prop('checked', false);
-		$("#mb").prop('checked', false);
+		$(".addon_toggle").prop('checked', false);
 	});
 	$("#version").change(function(){
 		version = $("#version").val();
+		$(".result").dialog("close");
 		reset_aspects();
 	});
 	$(".aspectlist").on( "click", ".aspect", function(){
@@ -135,7 +128,7 @@ $(function(){
 	$(".aspectlist").on("mouseenter", ".aspect", function() {
 		var aspect = $(this).attr("id");
 		if (aspect!="fire"&&aspect!="water"&&aspect!="order"&&aspect!="air"&&aspect!="entropy"&&aspect!="earth"){
-			var combination = version_dictionary[version]["combinations"][aspect];
+			var combination = combinations[aspect];
 			$("#combination_box #left").html('<img src="aspects/color/' + translate[combination[0]] + '.png" /><div class="name">' + translate[combination[0]] + '</div><div class="desc">' + combination[0] + '</div>');
 			$("#combination_box #right").html('<img src="aspects/color/' + translate[combination[1]] + '.png" /><div class="name">' + translate[combination[1]] + '</div><div class="desc">' + combination[1] + '</div>');
 			$("#combination_box #equals").html('<img src="aspects/color/' + translate[aspect] + '.png" /><div class="name">' + translate[aspect] + '</div><div class="desc">' + aspect + '</div>');
@@ -153,22 +146,20 @@ $(function(){
 		$(".result").dialog("close");
 	});
 	function reset_aspects() {
-		aspects = version_dictionary[version]["aspects"];
-		aspects.sort(function(a,b){
-			return (a == b) ? 0 : (translate[a]<translate[b]) ? -1 : 1;
-		});
-		$("#avail").empty();
+		aspects = $.extend([], version_dictionary[version]["base_aspects"]);
+		combinations = $.extend(true, {}, version_dictionary[version]["combinations"]);
+		$("#avail, #addons").empty();
 		$('#fromSel').ddslick("destroy");
 		$('#toSel').ddslick("destroy");
-		$("#fm").prop('checked', false);
-		$("#mb").prop('checked', false);
-		aspects.forEach(function(aspect) {
-			if (fm.indexOf(aspect) < 0 && mb.indexOf(aspect) < 0) {
-				$('#avail').append('<li class="aspect" id="'+aspect+'"><img src="aspects/color/' + translate[aspect] + '.png" /><div>' + translate[aspect] + '</div><div class="desc">' + aspect + '</div></li>');
-			} else {
-				$('#avail').append('<li class="aspect unavail" id="'+aspect+'"><img src="aspects/mono/' + translate[aspect] + '.png" /><div>' + translate[aspect] + '</div><div class="desc">' + aspect + '</div></li>');
-			}
+		$(".addon_toggle").prop('checked', false);
+		$.each(combinations, function(aspect, value){
+			aspects.push(aspect);
 		});
+		push_addons(aspects, combinations);
+		aspects.forEach(function(aspect) {
+			$('#avail').append('<li class="aspect" id="'+aspect+'"><img src="aspects/color/' + translate[aspect] + '.png" /><div>' + translate[aspect] + '</div><div class="desc">' + aspect + '</div></li>');
+		});
+		toggle_addons(addon_aspects);
 		var ddData = [];
 		aspects.forEach(function(aspect) {
 			ddData.push({text: translate[aspect], value: aspect, description: "(" + aspect + ")", imageSrc: "aspects/color/" + translate[aspect] + ".png"});
@@ -185,6 +176,11 @@ $(function(){
 			height: 300,
 			width: 170
 		});
+		graph={};
+		for (compound in combinations) {
+			connect(compound, combinations[compound][0]);
+			connect(compound, combinations[compound][1]);
+		}
 	}
 	function run() {
 		var fromSel = $('#fromSel').data('ddslick').selectedData.value;
@@ -192,9 +188,10 @@ $(function(){
 		var path = find(fromSel, toSel, steps.spinner("value"));
 		var id = fromSel+'to'+toSel;
 		var title = translate[fromSel]+' &rarr; '+translate[toSel];
-		var count={};
-		$.each(version_dictionary[version]["aspects"], function(aspect, value){
-			count[value]=0;
+		var step_count=0;
+		var aspect_count={};
+		$.each(aspects, function(aspect, value){
+			aspect_count[value]=0;
 		});
 		$('#'+id).remove();
 		$("body").append('<ul id="'+id+'" class="aspectlist result" title="'+title+'"></ul>');
@@ -205,18 +202,24 @@ $(function(){
 			width: 200
 		});
 		$('#'+id).append("<div></div>");
+		var loop_count=0;
 		path.forEach(function(e) {
-			count[e]++;
+			loop_count++;
+			if(loop_count!=1&&loop_count!=path.length) {
+				aspect_count[e]++;
+				step_count++;
+			}
 			$('#'+id).append('<li class="aspect_result"><img src="aspects/color/' + translate[e] + '.png" /><div>' + translate[e] + '</div><div class="desc">' + e + '</div></li><li>↓</li>');
 		});
 		$('#'+id).children().last().remove();
 		$('#'+id).append('<li id="aspects_used">Aspects Used</li>');
 		var used = '<ul id="aspects_used_list">';
-		$.each(count, function(aspect, value){
+		$.each(aspect_count, function(aspect, value){
 			if(value>0) {
 				used = $(used).append('<li title="'+translate[aspect]+': '+value+'" style="background-image:url(\'aspects/color/'+translate[aspect]+'.png\')">'+value+'</li>');
 			}
 		});
+		used = $(used).append("<div>Total Steps: "+ step_count+"</div>");
 		used = $(used).append('</ul>');
 		$('#'+id).append(used);
 		$('#'+id).dialog("open");
