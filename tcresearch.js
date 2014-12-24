3e1a740cc9de76bf30a29aa6301af654f515fcf3
Piotr Banasik <piotr.banasik@gmail.com>
2014-12-24 02:12:50 -0800

    Added support for TC 4.2.2.0

--- tcresearch.js
+++ tcresearch.js
@@ -1,5 +1,5 @@
 $(function(){
-	var latest_version = "4.1.1.14";
+	var latest_version = "4.2.2.0";
 	$.each(version_dictionary, function(key,version){
 		$("#version").append("<option value="+key+">"+key+"</option>");
 	});
