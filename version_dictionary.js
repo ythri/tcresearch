4a6332a98cab04dbd32aab52ceee4d1d687d5288
xgXFd9ct <lijohn@live.com.au>
2015-11-14 16:54:57 +1100

    Added Thaumcraft 5 data
    I might've missed a few.

--- version_dictionary.js
+++ version_dictionary.js
@@ -270,5 +270,46 @@ var version_dictionary = {
 			"crystal": ["earth", "order"],
 			"flight": ["air", "motion"]
 		}
-	}
+	},
+    "5.0.3":   {
+        "base_aspects":[
+        	"air",
+			"earth",
+			"fire",
+			"water",
+			"order",
+			"entropy"
+        ],
+        "combinations":{
+            "eldritch": ["void", "darkness"],
+            "aura": ["energy", "air"],
+            "aversion": ["entropy", "soul"],
+            "beast": ["motion", "life"],
+            "mind": ["fire", "soul"],
+            "wish": ["soul", "void"],
+            "undead": ["motion", "death"],
+            "craft": ["exchange", "tool"],
+            "cold": ["fire", "entropy"],
+            "plant": ["life", "earth"],
+            "man": ["soul", "life"],
+            "tool": ["metal", "energy"],
+            "light": ["air", "fire"],
+            "mechanism": ["motion", "tool"],
+            "metal": ["earth", "order"],
+            "death": ["water", "entropy"],
+            "motion": ["air", "order"],
+            "exchange": ["entropy", "order"],
+            "energy": ["order", "fire"],
+            "armor": ["soul", "earth"],
+            "senses": ["air", "soul"],
+            "soul": ["life", "death"],
+            "darkness": ["void", "light"],
+            "void": ["air", "entropy"],
+            "life": ["water", "earth"],
+            "trap": ["motion", "entropy"],
+            "taint": ["energy", "entropy"],
+            "crystal": ["earth", "air"],
+            "flight": ["air", "motion"]
+        }
+    }
 };
